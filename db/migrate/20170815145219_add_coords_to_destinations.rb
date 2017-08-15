62bb93ed0c687103770363182f258c6558574e3b
Cory Sturgis-Casturgi <casturgi@gmail.com>
2017-08-15 11:30:43 -0400

    initial commit

---
+++ db/migrate/20170815145219_add_coords_to_destinations.rb
@@ -0,0 +1,6 @@
+class AddCoordsToDestinations < ActiveRecord::Migration[5.1]
+  def change
+    add_column :destinations, :latitude, :float
+    add_column :destinations, :longitude, :float
+  end
+end
